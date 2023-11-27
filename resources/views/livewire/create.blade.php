dbe5788299c71f3299fce09140c31cee5ad07d21
waleedrehman31 <waleedrehman31@gmail.com>
2023-11-27 09:35:07 +0500

    create livewire frontend

---
+++ resources/views/livewire/create.blade.php
@@ -0,0 +1,26 @@
+<div class="card">
+    <div class="card-body">
+        <form>
+            <div class="form-group mb-3">
+                <label for="title">Title: </label>
+                <input type="text" id="title" class="form-control @error('title') is-invalid @enderror"
+                       placeholder="Enter Title" wire:model="title">
+                @error('title')
+                <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group mb-3">
+                <label for="description">Description: </label>
+                <textarea class="form-control @error('description') is-invalid @enderror" name="description"
+                          id="description" placeholder="Enter Description" wire:model="description"></textarea>
+                @error('description')
+                <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="d-grid gap-2">
+                <button wire:click.prevent="storePost()" class="btn btn-success btn-block">Save</button>
+                <button wire:click.prevent="cancelPost()" class="btn btn-secondary btn-block">Cancel</button>
+            </div>
+        </form>
+    </div>
+</div>
